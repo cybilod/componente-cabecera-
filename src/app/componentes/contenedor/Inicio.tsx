8baba81c4966380010d0579fed872b0df5fb775b
cybilod <rangelmaciel21@gmail.com>
2024-11-12 22:19:58 -0500

    compenentes cabecera e inicio

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,9 +1,67 @@
 export const Inicio = () => {
   return (
     <>
-      <br /><br />
-      Soy el Inicio <br />
-      Como el de la bienvenida...
+    <ul
+      className="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm"
+      id="pillNav2"
+      role="tablist"
+      style={{
+        "--bs-nav-link-color": "rgba(255, 255, 255, 0.75)",
+        "--bs-nav-link-hover-color": "#fff",
+        "--bs-nav-pills-link-active-color": "var(--bs-primary)",
+        "--bs-nav-pills-link-active-bg": "var(--bs-white)"
+      } as any} 
+    >
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link active rounded-5"
+          id="home-tab2"
+          data-bs-toggle="tab"
+          type="button"
+          role="tab"
+          aria-selected="true"
+        >
+          Inicio
+        </button>
+      </li>
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link rounded-5"
+          id="profile-tab2"
+          data-bs-toggle="tab"
+          type="button"
+          role="tab"
+          aria-selected="false"
+        >
+          Editoriales
+        </button>
+      </li>
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link rounded-5"
+          id="contact-tab2"
+          data-bs-toggle="tab"
+          type="button"
+          role="tab"
+          aria-selected="false"
+        >
+          Libros
+        </button>
+      </li>
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link rounded-5"
+          id="about-tab2"
+          data-bs-toggle="tab"
+          type="button"
+          role="tab"
+          aria-selected="false"
+        >
+          Acerca de
+        </button>
+      </li>
+    </ul>
+
     </>
   );
 };
--- src/app/componentes/otros/Acerca.tsx
+++ src/app/componentes/otros/Acerca.tsx
@@ -1,8 +1,33 @@
 export const Acerca = () => {
     return (
         <>
-        Soy el Acerca De <br />
-        El de la informacion...
+        <ul
+      className="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm"
+      id="pillNav2"
+      role="tablist"
+      style={{
+        "--bs-nav-link-color": "rgba(255, 255, 255, 0.75)",
+        "--bs-nav-link-hover-color": "#fff",
+        "--bs-nav-pills-link-active-color": "var(--bs-primary)",
+        "--bs-nav-pills-link-active-bg": "var(--bs-white)"
+      } as any} 
+    >
+     
+      
+      
+      <li className="nav-item" role="presentation">
+        <button
+          className="nav-link rounded-5"
+          id="about-tab2"
+          data-bs-toggle="tab"
+          type="button"
+          role="tab"
+          aria-selected="false"
+        >
+          Acerca de
+        </button>
+      </li>
+    </ul>
         </>
     )
 }
